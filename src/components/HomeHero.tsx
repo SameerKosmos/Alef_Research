0c9e3634bc4c23e813428a2ea13e06f0d07442f1
SameerKosmos <ecology.studies1978@gmail.com>
2026-09-06 04:30:27 +0000

    Update HomeHero.tsx

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -40,10 +40,10 @@ export default function HomeHero() {
           within hollowed asteroid cities and travel wherever these asteroid cities can be made to go.
         </p>
         <p>
-          Our techno-social systems combine a clean slate approach, rethinking about energy, bringing back of non-electric mechanical machines, 
+          Our techno-social systems combine a clean slate approach, rethink energy where most machines don't need electricty,
           have simplified engineering, decentralized production, biological innovation, bio-mimicry, circular production,
-          timeless design, gentleness, beauty, are buildable anywhere in the kosmos and have natural values including 
-          by abolishing feudalism, tax ( including money ), profit, class system, competition and tribalism.
+          timeless design, gentleness, beauty, are buildable anywhere in the kosmos and have natural values including by
+          abolishing feudalism, tax ( including money ), profit, class system, competition and tribalism.
         </p>
       </motion.div>
     </section>
